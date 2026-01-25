194cbd8a92d75c142034bea987dce068cb7f1284
Ananya Kharbanda <ananyakharbanda28@gmail.com>
2026-01-25 16:35:13 +0800

    ps2 q1

--- coursemology/ps2/ps2/Optimization.java
+++ coursemology/ps2/ps2/Optimization.java
@@ -2,10 +2,6 @@
  * The Optimization class contains a static routine to find the maximum in an array that changes direction at most once.
  */
 public class Optimization {
-
-    /**
-     * A set of test cases.
-     */
     static int[][] testCases = {
             {1, 3, 5, 7, 9, 11, 10, 8, 6, 4},
             {67, 65, 43, 42, 23, 17, 9, 100},
@@ -20,10 +16,38 @@ public class Optimization {
      * @return the maximum item in data Array
      */
     public static int searchMax(int[] dataArray) {
-        // TODO: Implement this
-        return 0;
-    }
+        if (dataArray == null) {
+            throw new IllegalArgumentException("Array cannot be null");
+        }
 
+        if (dataArray.length == 0) {
+            throw new IllegalArgumentException("Array must have at least one element");
+        }
+
+        if (dataArray.length == 1) {
+            return dataArray[0];
+        }
+
+        int n = dataArray.length;
+
+        if (dataArray[0] < dataArray[1] && dataArray[n - 2] > dataArray[n - 1]) {
+            int left = 0;
+            int right = n - 1;
+
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+
+                if (dataArray[mid] < dataArray[mid + 1]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            return dataArray[left];
+        }
+
+        return Math.max(dataArray[0], dataArray[n - 1]);
+    }
     /**
      * A routine to test the searchMax routine.
      */
